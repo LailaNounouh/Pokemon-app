9329532f322956a09a01bfa84abdd559564fb0e7
LailaNounouh <lailanounouh@gmail.com>
2025-05-21 17:14:01 +0200

    ophalen van lijst van pokemons via api

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 let allePokemon = [];
 let gekozenType = '';
 let favorieten = JSON.parse(localStorage.getItem('favorieten') || '[]');
+async function haalPokemonDetails(url) {
+  const res = await fetch(url);
+  return await res.json();
+}
+async function haalPokemonLijst() {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
+  const data = await res.json();
+  return data.results;
+}
+
 
